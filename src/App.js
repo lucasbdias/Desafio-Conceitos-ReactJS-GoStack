ccbc895be6ce6c72062917b017c1155a4a8c3e01
lucasbdias <lucasbrzdias@gmail.com>
2020-09-02 06:14:45 -0300

    listagem e criação de repositórios feitos

--- src/App.js
+++ src/App.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+  const [repositories, setRepositories] = useState([]);
 
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: "Novo repositório",
+	    url: "https://github.com/lucasbdias/Conceitos-do-NodeJS-GoStack",
+	    techs: ["Node.js", "JS"],
+	    likes: 0
+    });
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+  }
+ 
   async function handleRemoveRepository(id) {
-    // TODO
+    //const response = await api.delete('repositories/' + id)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
+      {repositories.map(repository => <li key={repository.id}>{repository.title} <button onClick={() => handleRemoveRepository(repository.id)}>Remover</button></li>)}
         <li>
           Repositório 1
 
